97b972c65cde8a3f83dbf9bc9ca74419418b2e68
quanhuy38 <157669413+quanhuy38@users.noreply.github.com>
2025-03-09 20:51:42 +0700

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-#dùng: export PYTORCH_ENABLE_MPS_FALLBACK=1 trong terminal
-import OCRmodel as ocr
-image_path = '/Users/lequanhuy/Documents/Code/Visual Code/Projects/HandwritingOCR/captured_images/captured_image.jpg'
-print(ocr.OCRing(image_path))
