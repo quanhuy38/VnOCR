b0f1349ab12358db59be1093846cdbd0e7e81664
quanhuy38 <157669413+quanhuy38@users.noreply.github.com>
2025-03-09 20:55:40 +0700

    Update ImagesProcessing.py

--- ImagesProcessing.py
+++ ImagesProcessing.py
@@ -11,20 +11,9 @@ def show_image(img):
     plt.axis('off')
     plt.show()
 def save_processed_image(img):
-    output_path = "Projects/HandwritingOCR/processed_images/processed_image.jpg"
+    output_path = "processed_images/processed_image.jpg"
     cv2.imwrite(output_path, img)
     return output_path
-'''def createBoundingBox(img): 
-    ocr_data = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT)
-    n_boxes = len(ocr_data['level'])
-    for i in range(n_boxes):
-        if ocr_data['level'][i] == 3:
-            (x, y, w, h) = (ocr_data['left'][i], ocr_data['top'][i], ocr_data['width'][i], ocr_data['height'][i])
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 5)
-    plt.imshow(img, cmap='gray')
-    plt.axis('off')
-    plt.show()
-'''
 
 def super_resolution(img):
     model = EdsrModel.from_pretrained('eugenesiow/edsr-base', scale=2)
@@ -32,7 +21,7 @@ def super_resolution(img):
     inputs = ImageLoader.load_image(pil_img)
     preds = model(inputs)
 
-    ImageLoader.save_image(preds, 'Projects/HandwritingOCR/processed_images/processed_image.jpg') 
+    ImageLoader.save_image(preds, 'processed_images/processed_image.jpg') 
 def process_image(image_path):
     img = preprocess_image(image_path)
     super_resolution(img)
